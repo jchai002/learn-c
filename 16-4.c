e2cb3786c4340e38533236d0fc7acac3ceb9ebe2
Jerry Chai <jchai002@gmail.com>
2015-12-04 15:58:47 -0800

    finish 16-4

---
+++ 16-4.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+/* function definition */
+void StrPrint(char **str)
+{
+  int i;
+  for (i=0; i<7; i++) {
+  printf("%s\n", str[i]);
+  }
+}
+
+main()
+{
+char *str[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+};
+
+StrPrint(str);
+
+return 0;
+}
